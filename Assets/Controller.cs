224e0b3271b6053c56960ed0fbb3bd5913064c51
Fillipe Ogg <dupuqub@gmail.com>
2019-01-13 13:28:34 -0200

    More XBOX360 gamepad mapping.

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -11,19 +11,43 @@ public class Controller : MonoBehaviour
     // Get every connected joystick
     joysticks = Input.GetJoystickNames();
 
-    // Show every connected joystick (uncomment below)
-    // foreach (string unit in joysticks) Debug.Log(unit);
+    // Show every connected joystick
+    foreach (string unit in joysticks) Debug.Log(unit);
 
-    // Buttons
-    if(Input.GetButton("ButtonA")) Debug.Log("ButtonA");
-    if(Input.GetButton("ButtonB")) Debug.Log("ButtonB");
-    if(Input.GetButton("ButtonX")) Debug.Log("ButtonX");
-    if(Input.GetButton("ButtonY")) Debug.Log("ButtonY");
+    //..........................................................................
+    // XBOX360 setup
 
-    // Pad
+    // Colors
+    if(Input.GetButton("A")) Debug.Log("A");
+    if(Input.GetButton("B")) Debug.Log("B");
+    if(Input.GetButton("X")) Debug.Log("X");
+    if(Input.GetButton("Y")) Debug.Log("Y");
+
+    // Uppers
+    if(Input.GetButton("LB")) Debug.Log("LB");
+    if(Input.GetButton("RB")) Debug.Log("RB");
+
+    // Options
+    if(Input.GetButton("Back")) Debug.Log("Back");
+    if(Input.GetButton("Start")) Debug.Log("Start");
+
+    // Sticks
+    if(Input.GetButton("LS")) Debug.Log("LS");
+    if(Input.GetButton("RS")) Debug.Log("RS");
+
+    // Get angle of stick left
+    // Get angle of stick right
+
+    // Arrows
     if(Input.GetAxis("PadHor") < 0) Debug.Log("PadLeft");
     else if(Input.GetAxis("PadHor") > 0) Debug.Log("PadRight");
     if(Input.GetAxis("PadVer") < 0) Debug.Log("PadBottom");
     else if(Input.GetAxis("PadVer") > 0) Debug.Log("PadTop");
+
+    // Triggers
+    // Get strength of trigger left
+    // Get strength of trigger right
+
+    //..........................................................................
   }
 }
