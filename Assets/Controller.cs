c466d1be4981306c03f1e72025aa13e8fbcd47fe
Fillipe Ogg <dupuqub@gmail.com>
2019-01-13 13:02:57 -0200

    First commit.

---
+++ Assets/Controller.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+  public string[] joysticks;
+
+  void Update()
+  {
+    // Get every connected joystick
+    joysticks = Input.GetJoystickNames();
+
+    // Show every connected joystick (uncomment below)
+    // foreach (string unit in joysticks) Debug.Log(unit);
+
+    // Buttons
+    if(Input.GetButton("ButtonA")) Debug.Log("ButtonA");
+    if(Input.GetButton("ButtonB")) Debug.Log("ButtonB");
+    if(Input.GetButton("ButtonX")) Debug.Log("ButtonX");
+    if(Input.GetButton("ButtonY")) Debug.Log("ButtonY");
+
+    // Pad
+    if(Input.GetAxis("PadHor") < 0) Debug.Log("PadLeft");
+    else if(Input.GetAxis("PadHor") > 0) Debug.Log("PadRight");
+    if(Input.GetAxis("PadVer") < 0) Debug.Log("PadBottom");
+    else if(Input.GetAxis("PadVer") > 0) Debug.Log("PadTop");
+  }
+}
---
+++ Assets/Sketch.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Main : MonoBehaviour
+{
+  public GameObject myCamera;
+  public GameObject capsuleObject;
+  public Rigidbody capsule;
+  public float speed = 2.0f;
+
+  void Start()
+  {
+    myCamera = GameObject.Find("Camera");
+    capsuleObject = GameObject.Find("Capsule");
+    capsule = capsuleObject.GetComponent<Rigidbody>();
+
+    Physics.gravity = new Vector3(0, -100.0f, 0);
+  }
+
+  // void FixedUpdate()
+  // {
+  //   capsule.AddForce(1000.0f, 2000.0f, 3000.0f);
+
+  //   float interpolation = speed * Time.deltaTime;
+    
+  //   Vector3 position = myCamera.transform.position;
+  //   position.x = Mathf.Lerp(myCamera.transform.position.x, capsuleObject.transform.position.x, interpolation);
+  //   position.y = Mathf.Lerp(myCamera.transform.position.y, capsuleObject.transform.position.y, interpolation);
+  //   position.z = Mathf.Lerp(myCamera.transform.position.z, capsuleObject.transform.position.z, interpolation);
+
+  //   myCamera.transform.position = position;
+  // }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
