be2ae1c2af6d7e11bed579b9fef7285492872d5d
Fillipe Ogg <dupuqub@gmail.com>
2019-01-14 01:53:11 -0200

    Ship rotation tests + GamepadXbox360 class

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour
+{
+  void FixedUpdate()
+  {
+    GamepadXbox360 gamepad = new GamepadXbox360();
+    float delta = Time.deltaTime;
+
+    if(gamepad.LSX != 0) transform.Rotate(Vector3.up, gamepad.LSX * delta * 100f);
+    if(gamepad.LSY != 0) transform.Rotate(Vector3.left, gamepad.LSY * delta * 100f);
+
+    if(!gamepad.LB && gamepad.RB) transform.Rotate(Vector3.forward, delta * 100f);
+    else if(gamepad.LB && !gamepad.RB) transform.Rotate(Vector3.back, delta * 100f);
+  }
+}
