f1b0565b2dd557e0a1a94920e94e4c657344c715
Fillipe Ogg <dupuqub@gmail.com>
2019-01-15 05:49:47 -0200

    Spaces in GamepadXbox360 + Sketch.cs deletion

--- Assets/Scripts/GamepadXbox360.cs
+++ Assets/Scripts/GamepadXbox360.cs
@@ -2,31 +2,31 @@ using UnityEngine;
 
 public class GamepadXbox360
 {
-// Colors.
-public bool A = Input.GetButton("A");
-public bool B = Input.GetButton("B");
-public bool X = Input.GetButton("X");
-public bool Y = Input.GetButton("Y");
+  // Colors.
+  public bool A = Input.GetButton("A");
+  public bool B = Input.GetButton("B");
+  public bool X = Input.GetButton("X");
+  public bool Y = Input.GetButton("Y");
 
-// Options.
-public bool BACK = Input.GetButton("Back");
-public bool START = Input.GetButton("Start");
+  // Options.
+  public bool BACK = Input.GetButton("Back");
+  public bool START = Input.GetButton("Start");
 
-// Sticks.
-public bool LSB = Input.GetButton("LSB");
-public bool RSB = Input.GetButton("RSB");
-public float LSX = Input.GetAxis("LSX"); // -1 to 1.
-public float LSY = Input.GetAxis("LSY"); // -1 to 1.
-public float RSX = Input.GetAxis("RSX"); // -1 to 1.
-public float RSY = Input.GetAxis("RSY"); // -1 to 1.
+  // Sticks.
+  public bool LSB = Input.GetButton("LSB");
+  public bool RSB = Input.GetButton("RSB");
+  public float LSX = Input.GetAxis("LSX"); // -1 to 1.
+  public float LSY = Input.GetAxis("LSY"); // -1 to 1.
+  public float RSX = Input.GetAxis("RSX"); // -1 to 1.
+  public float RSY = Input.GetAxis("RSY"); // -1 to 1.
 
-// Uppers.
-public bool LB = Input.GetButton("LB");
-public bool RB = Input.GetButton("RB");
-public float LT = Input.GetAxis("LT"); // 0 to 1.
-public float RT = Input.GetAxis("RT"); // 0 to 1.
+  // Uppers.
+  public bool LB = Input.GetButton("LB");
+  public bool RB = Input.GetButton("RB");
+  public float LT = Input.GetAxis("LT"); // 0 to 1.
+  public float RT = Input.GetAxis("RT"); // 0 to 1.
 
-// Arrows.
-public float PADX = Input.GetAxis("PadX"); // -1 to 1.
-public float PADY = Input.GetAxis("PadY"); // -1 to 1.
+  // Arrows.
+  public float PADX = Input.GetAxis("PadX"); // -1 to 1.
+  public float PADY = Input.GetAxis("PadY"); // -1 to 1.
 }
