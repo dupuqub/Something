4dd02843131796c6a3baf89cd138e7fa43576f27
Fillipe Ogg <dupuqub@gmail.com>
2019-01-15 07:10:24 -0200

    Hotfix at CameraFollow.cs

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -8,9 +8,9 @@ public class CameraFollow : MonoBehaviour
   {
     GamepadXbox360 gamepad = new GamepadXbox360();
     float delta = Time.deltaTime;
-    string horizontal = gamepad.LB ? "Left" : gamepad.RB ? "Right" : "";
-    string vertical = gamepad.LSY > 0 ? "Top" : gamepad.LSY < 0 ? "Bottom" : "";
     float diagonal = gamepad.LSX != 0 ? gamepad.LSX * 15f : 0;
+    string vertical = gamepad.LSY > 0 ? "Top" : gamepad.LSY < 0 ? "Bottom" : "";
+    string horizontal = gamepad.LB && !gamepad.RB ? "Left" : !gamepad.LB && gamepad.RB ? "Right" : "";
 
     GameObject guide = GameObject.Find("CameraGuide" + horizontal + vertical);
     Vector3 guideVect = guide.transform.position;
