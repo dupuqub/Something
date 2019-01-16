157f94a44bb719b1ef897c32330f5792de6766c4
Fillipe Ogg <dupuqub@gmail.com>
2019-01-16 20:18:11 -0200

    More polished camera through guide's localPosition movement.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -10,20 +10,14 @@ public class CameraFollow : MonoBehaviour
     float delta = Time.deltaTime;
 
     string axisX = gamepad.LB && !gamepad.RB ? "Left" : !gamepad.LB && gamepad.RB ? "Right" : "";
-    string axisY = gamepad.LSY > 0 ? "Top" : gamepad.LSY < 0 ? "Bottom" : "";
     float axisZ = gamepad.LSX != 0 ? gamepad.LSX * 15f : 0;
 
-    GameObject guide = GameObject.Find("CameraGuide" + axisX + axisY);
+    GameObject guide = GameObject.Find("CameraGuide" + axisX);
     Vector3 guideVect = guide.transform.position;
     Vector3 guideEuler = guide.transform.rotation.eulerAngles;
     Quaternion guideQuat = Quaternion.Euler(guideEuler.x, guideEuler.y, guideEuler.z + axisZ);
 
     transform.position = Vector3.Lerp(transform.position, guideVect, delta * 10f);
     transform.rotation = Quaternion.Lerp(transform.rotation, guideQuat, delta * 10f);
-
-    // Notes.
-    // Ship going down should be on top of the screen (point is too low! Watch for the issue below after fixing).
-    // Ship going up should be less inclined (is point too high?).
-    // Ship rolling is possibly snapping too hard.
   }
 }
