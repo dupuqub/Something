9ff7cd950c89e0c516a8182d5e2f7e0ce7703dac
Fillipe Ogg <dupuqub@gmail.com>
2019-01-15 22:45:27 -0200

    Aim + clean up at CameraFollow.cs.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -8,17 +8,18 @@ public class CameraFollow : MonoBehaviour
   {
     GamepadXbox360 gamepad = new GamepadXbox360();
     float delta = Time.deltaTime;
-    float diagonal = gamepad.LSX != 0 ? gamepad.LSX * 15f : 0;
-    string vertical = gamepad.LSY > 0 ? "Top" : gamepad.LSY < 0 ? "Bottom" : "";
-    string horizontal = gamepad.LB && !gamepad.RB ? "Left" : !gamepad.LB && gamepad.RB ? "Right" : "";
 
-    GameObject guide = GameObject.Find("CameraGuide" + horizontal + vertical);
+    string axisX = gamepad.LB && !gamepad.RB ? "Left" : !gamepad.LB && gamepad.RB ? "Right" : "";
+    string axisY = gamepad.LSY > 0 ? "Top" : gamepad.LSY < 0 ? "Bottom" : "";
+    float axisZ = gamepad.LSX != 0 ? gamepad.LSX * 15f : 0;
+
+    GameObject guide = GameObject.Find("CameraGuide" + axisX + axisY);
     Vector3 guideVect = guide.transform.position;
     Vector3 guideEuler = guide.transform.rotation.eulerAngles;
-    Quaternion newQuat = Quaternion.Euler(guideEuler.x, guideEuler.y, guideEuler.z + diagonal);
+    Quaternion guideQuat = Quaternion.Euler(guideEuler.x, guideEuler.y, guideEuler.z + axisZ);
 
     transform.position = Vector3.Lerp(transform.position, guideVect, delta * 10f);
-    transform.rotation = Quaternion.Lerp(transform.rotation, newQuat, delta * 10f);
+    transform.rotation = Quaternion.Lerp(transform.rotation, guideQuat, delta * 10f);
 
     // Notes.
     // Ship going down should be on top of the screen (point is too low! Watch for the issue below after fixing).
